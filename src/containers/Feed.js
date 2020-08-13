e861483c3baf97022eb2fbb28a9d2b6774242b9b
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-08-13 16:23:47 -0400

    add pagination

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import queryString from "query-string";
+
 import Card from "../components/Card/Card";
 
 const FeedWrapper = styled.div`
@@ -13,22 +16,44 @@ const Alert = styled.div`
   text-align: center;
 `;
 
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const PaginationBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PaginationLink = styled(Link)`
+  padding: 1%;
+  background: lightBlue;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+`;
+
 const ROOT_API = "https://api.stackexchange.com/2.2/";
 
 class Feed extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const query = queryString.parse(props.location.search);
     this.state = {
       data: [],
+      page: query.page ? parseInt(query.page) : 1,
       loading: true,
       error: "",
     };
   }
 
-  async componentDidMount() {
+  async fetchAPI(page) {
     try {
+      const stringPage = page ? `&page=${page}` : ``;
       const data = await fetch(
-        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`
+        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${stringPage}`
       );
       const dataJSON = await data.json();
 
@@ -46,9 +71,28 @@ class Feed extends Component {
     }
   }
 
-  render() {
-    const { data, loading, error } = this.state;
+  async componentDidMount() {
+    const { page } = this.state;
+    this.fetchAPI(page);
+  }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.search !== this.props.location.search) {
+      const query = queryString.parse(this.props.location.search);
+      this.setState(
+        {
+          page: parseInt(query.page),
+        },
+        () => {
+          return this.fetchAPI(this.state.page);
+        }
+      );
+    }
+  }
+
+  render() {
+    const { data, loading, error, page } = this.state;
+    const { match } = this.props;
     if (loading || error) {
       return <Alert>{loading ? "Loading..." : error}</Alert>;
     }
@@ -56,8 +100,21 @@ class Feed extends Component {
     return (
       <FeedWrapper>
         {data.items.map((item) => (
-          <Card key={item.question_id} data={item} />
+          <CardLink
+            to={`/questions/${item.question_id}`}
+            key={item.question_id}
+          >
+            <Card data={item} />
+          </CardLink>
         ))}
+        <PaginationBar>
+          <PaginationLink to={`${match.url}?page=${page <= 1 ? 1 : page - 1}`}>
+            Previous
+          </PaginationLink>
+          <PaginationLink to={`${match.url}?page=${page + 1}`}>
+            Next
+          </PaginationLink>
+        </PaginationBar>
       </FeedWrapper>
     );
   }
